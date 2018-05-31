b455d0c1fd4248f0ada0a3a128fd18f659aa0c39
pdnghiaqoi <31226956+pdnghiaqoi@users.noreply.github.com>
2018-05-31 18:15:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var ball = [
 
 bot.on("ready",function() {
     console.log("Cool! My bot is ready!")
-    bot.user.setActivity("/help.Still WIP!", {type: "WATCHING"});
+    bot.user.setGame("/help. Still in WIP!")
 })
 
 
@@ -57,11 +57,34 @@ bot.on('message', (message) => {
            .addField("/8balls", "Let the 8 balls answer your question!")
            .addField("/ping", "ping pong")
            .addField("/pong", "pong ping")
-           .addField("Tip:You can also use the commands if you are dm with me!", "<@419400751098494978>")
+           .addField("/despacito", "some despacito meme")
            .setColor(0x00FFFF)
            .setFooter("List of commands")
         message.author.sendEmbed(embed);
         break;
+        p1 = "./meme/1.jps"; p2 = "./meme/2.jps"; p3 = "./meme/3.jps"; p4 = "./meme/4.jps"; p5 = "./meme/5.jps"; p6 = "./meme/6.jps"; p7 = "./meme/7.jps"; p8 = "./meme/8.jps"; p9 = "./meme/9.jps"; p10 = "./meme/10.jps"; p11 = "./meme/11.jps"; p12 = "./meme/12.jps"; p13 = "./meme/1.jps"; p1 = "./meme/13.jps"; p14 = "./meme/14.png"; p15 = "./meme/15.png"; 
+        case "despacito":
+        number = 15
+        var random = Math.floor (Math.random() * (number - 1 + 1)) + 1
+        switch (random) {
+            case 1: message.channel.send ({ files: [p1] }); break;
+            case 2: message.channel.send ({ files: [p2] }); break;
+            case 3: message.channel.send ({ files: [p3] }); break;
+            case 4: message.channel.send ({ files: [p4] }); break;
+            case 5: message.channel.send ({ files: [p5] }); break;
+            case 6: message.channel.send ({ files: [p6] }); break;
+            case 7: message.channel.send ({ files: [p7] }); break;
+            case 8: message.channel.send ({ files: [p8] }); break;
+            case 9: message.channel.send ({ files: [p9] }); break;
+            case 10: message.channel.send ({ files: [p10] }); break;
+            case 11: message.channel.send ({ files: [p11] }); break;
+            case 12: message.channel.send ({ files: [p12] }); break;
+            case 13: message.channel.send ({ files: [p13] }); break;
+            case 14: message.channel.send ({ files: [p14] }); break;
+            case 15: message.channel.send ({ files: [p15] }); break;    
+        }
+
+        break; 
         default:
         message.channel.sendMessage("Invaild commands! ourbotisgettinginvailded");
         break;
