0f90677c7426350d03263a47c8175dc3b1e344f6
pdnghiaqoi <31226956+pdnghiaqoi@users.noreply.github.com>
2018-05-31 10:19:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const Discord = require('discord.js');
+const YTDL = require("ytdl-core")
 const bot = new Discord.Client();
 
+
 var prefix = "/"
+
+function play(connection, message) {
+    var server = servers[message.guild.id];
+
+    server.dispatcher = connection.playStream(YTDL(server.quene[0], {filter: "audioonly"}));
+
+    server.quene.shift();
+
+    server.dispatcher.on("end", function() {
+         if (server.quene[0]) play(connection, message);
+         else connection.disconnect();
+    });
+}
+var servers = {};
+
 
 var ball = [
     "oh yes",
@@ -42,6 +59,47 @@ bot.on('message', (message) => {
         case "loader":
         message.channel.sendMessage("Your loader is require(992103297). Thank you for using us!")
         break;
+        case "playsnd":
+             if (!args[1]) {
+                 message.channel.sendMessage('hmmm.... don have link?');
+                 return;
+             };
+
+             if (!message.member.voiceChannel) {
+                message.channel.sendMessage('pls go into voice channel');
+                return;
+            }
+
+            if(!servers[message.guild.id]) servers[message.guild.id] = {
+                quene: []
+            };
+
+            var server = servers[message.guild.id];
+
+            server.quene.push(args[1]);
+
+            if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+              play(connection, message);
+            });
+
+        break;
+        case "skip":
+        var server = servers[message.guild.id];
+
+        if (server.dispatcher) server.dispatcher.end();
+        break;
+        case "stop":
+        var server = servers[message.guild.id];
+
+        if (message.guild.voiceConnection)
+        {
+            for (var i = server.queue.length - 1; i >= 0; i--) 
+            {
+                server.queue.splice(i, 1);
+         }
+            server.dispatcher.end();
+        }        break;
+
         case "help":
         var embed = new Discord.RichEmbed()
            .addField("/help", "Show this.")
@@ -50,6 +108,9 @@ bot.on('message', (message) => {
            .addField("/8balls", "Let the 8 balls answer your question!")
            .addField("/ping", "ping pong")
            .addField("/pong", "pong ping")
+           .addField("/playsnd", "Play/add a song to quene. (YOU MUST BE IN VOICE CHANNEL, AND PUT A LINK.)")
+           .addField("/skip", "Skip a song to quene")
+           .addField("/stop", "Stop a song.")
            .setColor(0x00FFFF)
            .setFooter("List of commands")
         message.author.sendEmbed(embed);
