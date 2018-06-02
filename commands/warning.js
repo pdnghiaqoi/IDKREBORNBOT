1eb0336b9c080f17ee25355d93c31117c984ec3b
pdnghiaqoi <31226956+pdnghiaqoi@users.noreply.github.com>
2018-06-02 10:23:44 +0700

    Update warning.js

--- commands/warning.js
+++ commands/warning.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if (!message.member.roles.some(r=>["Creator of this shit admin","SB Mods"].includes(r.name))) return; message.author.sendMessage("You can't use this!")
+    if (!message.member.roles.some(r=>["Warning useable"].includes(r.name))) return;
     if(!rUser) return message.author.send("Wait, the user didn't exist! Make sure to type the correct name,and it need to exist in our group!");
     let rreason = args.join(" ").slice(22);
 
