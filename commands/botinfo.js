bd7c8229c1694e40b78ee500e152226e1e4408f3
pdnghiaqoi <31226956+pdnghiaqoi@users.noreply.github.com>
2018-06-02 09:38:25 +0700

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let bicon = bot.user.displayAvatarURL;
     let botembed = new Discord.RichEmbed()
-    .setColor("#15f153")
+    .setColor("#0x00FFFF")
     .setThumbnail(bicon)
     .addField("What your name?", `My name is ${bot.user.username}, and nice to meet you!`)
     .addField("When did you created?",`I was created at ${bot.user.createdAt}`)
