b44f4da9b8b12bd803a1974a20ae6d803887f919
pdnghiaqoi <31226956+pdnghiaqoi@users.noreply.github.com>
2018-06-03 21:45:58 +0700

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,22 +1,24 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(kUser == bot.user.username) return; 
     if(!kUser) return message.channel.send("Can't find user!");
     let kReason = args.join(" ").slice(22);
-    if(!message.member.roles.some(r=>["Creator of this shit admin", "BOT", "super kiki", "Bot Admin","Trusted Players/Normal Friend","BOT(Snappy) Creator","Staff","SB Mods"].includes(r.name))) return message.channel.send("You don't have perm to kick!");
-    if(kUser.roles.some(r=>["Creator of this shit admin", "BOT", "super kiki", "Bot Admin","Trusted Players/Normal Friend","BOT(Snappy) Creator","Staff","SB Mods"].includes(r.name))) return message.channel.send("You can't kick him!");
+    if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You don't have perm to kick!");
+    if(kUser.hasPermission("KICK_MEMBERS")) return message.channel.send("You can't kick him!");
 
     let kickEmbed = new Discord.RichEmbed()
     .setDescription("~Kick~")
-    .setColor("#e56b00")
+    .setColor("#0x00FFFF")
     .addField("A user just got kicked!", `${kUser} with ID ${kUser.id} was got kicked`)
     .addField(`Who kicked ${kUser}?`, `<@${message.author.id}> with ID ${message.author.id} just kick ${kUser}.`)
     .addField(`Where ${kUser} was got kicked?`, `${kUser} was got kicked in ${message.channel}`)
     .addField(`When ${kUser} was got kicked? `, `${kUser} was got kicked when it message.createdAt`)
     .addField(`Why ${kUser} was got kicked?`, `${kUser} was got kicked because ${kReason}`);
 
-    let kickChannel = message.guild.channels.find(`name`, "bots-log");
+    let kickChannel = message.guild.channels.find(`name`, "kick-log");
     if(!kickChannel) return message.channel.send("Can't find bot log channel.");
 
     message.guild.member(kUser).kick(kReason);
