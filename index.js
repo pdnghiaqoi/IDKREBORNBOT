d38f8a42ad6342ed11f940a5d02f55d7de06f992
pdnghiaqoi <31226956+pdnghiaqoi@users.noreply.github.com>
2018-06-19 14:56:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("ready", async () => {
   bot.user.setStatus('idle')
 
-  bot.user.setGame('/how. Still in WIP!', 'https://www.twitch.tv/pdnghia')
+  bot.user.setGame('Fortnite', 'https://www.twitch.tv/pdnghia')
    
 });
 
