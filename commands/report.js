360152da5d4665aac0790abe8251b1e2384ca3c2
pdnghiaqoi <31226956+pdnghiaqoi@users.noreply.github.com>
2018-06-20 15:02:40 +0700

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,34 +1,32 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!rUser) return message.channel.send("where da hell is that person? where am i?");
+        if (message.channel.id === '458901096564260864') {
     let rreason = args.join(" ").slice(22);
 
     let reportEmbed = new Discord.RichEmbed()
-    .setDescription("Twin brother of <@407179814974259200>")
+    .setDescription("Report Info:")
     .setColor("#0x00FFFF")
-    .addField("A user was got reported!", `${rUser} with ID: ${rUser.id}`)
-    .addField("Who report him?", `${message.author} with ID: ${message.author.id}`)
-    .addField("Where he got reported?", message.channel)
-    .addField("When he got reported?", message.createdAt)
-    .addField("Why he got reported/Proof?", rreason);
+    .addField("Reporter:", `${message.author} with ID: ${message.author.id}`)
+    .addField("Reason/Proof:", rreason)
+    .addField("Time:", message.createdAt);
     let authorRe = new Discord.RichEmbed()
-    .setDescription("Your report was sended into reports channel and will resolve soon!")
+    .setDescription("Your report was sended into recently-reports channel and will resolve soon!")
     .setColor("#0x00FFFF")
     .setFooter("Review")
-    .addField("You was reported a user named", `${rUser} with ID: ${rUser.id}`)
-    .addField("Where you report him:", message.channel)
-    .addField("When you report him:", message.createdAt)
-    .addField("Your reason/proof:", rreason);
+    .addField("Your report's channel", message.channel)
+    .addField("Your reason/proof:", rreason)
+    .addField("Time:", message.createdAt);
 
-    let reportschannel = message.guild.channels.find(`name`, "reports");
+    let reportschannel = message.guild.channels.find(`name`, "recently-reports");
     if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
     message.author.sendEmbed(authorRe);
     message.delete().catch(O_o=>{});
     reportschannel.send(reportEmbed);
 
-}
+} else {
+  message.channel.sendMessage("sir, this commands is illegal in this channel. please go to <#458901096564260864> to use this command.")
+}}
  
 module.exports.help = {
   name: "report"
