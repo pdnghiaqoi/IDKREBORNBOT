e6063ba6e4868c3fef11fe363dbea7c66aacca0b
pdnghiaqoi <31226956+pdnghiaqoi@users.noreply.github.com>
2018-07-04 12:37:47 +0700

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -2,27 +2,26 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
      if (message.channel.name === 'reports') {
-    let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!rUser) return message.channel.send("where da hell is that person? where am i?");
-    let rreason = args.join(" ").slice(22);
-
+    let rreason = args.slice(1).join(" ");
+    var Attachment = (message.attachments).array();
+    if (!Attachment) return message.channel.send("where are my proof?")
+    var ure = Attachment[0].url
     let reportEmbed = new Discord.RichEmbed()
-    .setDescription("Twin brother of <@407179814974259200>")
+    .setDescription("Report:")
     .setColor("#0x00FFFF")
-    .addField("A user was got reported!", `${rUser} with ID: ${rUser.id}`)
-    .addField("Who report him?", `${message.author} with ID: ${message.author.id}`)
-    .addField("Where he got reported?", message.channel)
-    .addField("When he got reported?", message.createdAt)
-    .addField("Why he got reported/Proof?", rreason);
+    .addField("Reporter:", `${message.author} with ID: ${message.author.id}`)
+    .addField("Reporter's Channel:", message.channel)
+    .addField("Reporter's Time:", message.createdAt)
+    .addField("Reporter's Reason:", rreason)
+    .setImage(ure);
     let authorRe = new Discord.RichEmbed()
     .setDescription("Your report was sended into reports and will resolve soon!")
     .setColor("#0x00FFFF")
     .setFooter("Review")
-    .addField("You was reported a user named", `${rUser} with ID: ${rUser.id}`)
-    .addField("Where you report him:", message.channel)
-    .addField("When you report him:", message.createdAt)
-    .addField("Your reason/proof:", rreason);
-
+    .addField("Your report's channel:", message.channel)
+    .addField("Your report's time:", message.createdAt)
+    .addField("Your report's reason:", rreason)
+    .setImage(ure);
     let reportschannel = message.guild.channels.find(`name`, "recently-reports");
     if(!reportschannel) return message.channel.send("Couldn't find reports channel.");
     message.author.sendEmbed(authorRe);
