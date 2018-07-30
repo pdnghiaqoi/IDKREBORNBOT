4c35d64b1864ce5d5407817fbfc652f565203901
pdnghiaqoi <31226956+pdnghiaqoi@users.noreply.github.com>
2018-07-30 08:03:44 +0700

    Update exe.js

--- commands/exe.js
+++ commands/exe.js
@@ -1,13 +1,16 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
       if(!message.member.hasPermission("ADMINISTRATOR")) return;
-      const code = args.join(" ");
+      var code = args.join(" ");
 try {
     eval(code); 
 } catch (e) {
     if (e instanceof SyntaxError) {
         message.channel.send("error:${e.message}");
     }
+      else {
+     message.channel.send("exe success")       
+      }
 }
 }
 module.exports.help = {
